63ebc69db3b59b101f9e57d6357cf44431530818
Aida <45454055+a1da4@users.noreply.github.com>
2021-02-03 14:58:54 +0900

    Update create_pretraining_data.sh
    fix path

--- create_pretraining_data.sh
+++ create_pretraining_data.sh
@@ -1,8 +1,8 @@
 # max_seq_length だけ256にした
 python3 create_pretraining_data.py \
-    --input_file ~/work/lsm/data/japanese-magazine/word_replace-noun/pre-post-war.txt \
-    --output_file ~/work/lsm/data/japanese-magazine/word_replace-noun/pretraining-data.tf-record \
-    --vocab_file ~/work/lsm/data/japanese-magazine/word_replace-noun/vocab.txt \
+    --input_file /path/to/data/data.txt \
+    --output_file /path/to/data/pretraining-data.tf-record \
+    --vocab_file /path/to/vocab/vocab.txt \
     --subword_type bpe \
     --max_seq_length 256 \
     --max_predictions_per_seq 20 \
